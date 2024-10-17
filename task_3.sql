6f488954c208281ad7bf70ffd98ae25c446cec4a
Lwandoxelo27 <138695827+Lwandoxelo27@users.noreply.github.com>
2024-10-17 18:59:30 +0200

    added script

---
+++ task_3.sql
@@ -0,0 +1,10 @@
+-- task_3.sql
+
+SHOW TABLES;
+
+
+To run this script and list all tables in the alx_book_store database, use the following MySQL command:
+
+
+bash
+mysql -u your_username -pyour_password -D alx_book_store < task_3.sql
