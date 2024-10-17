e7981da00111cab930927d41b4bbc868c367e4d4
Lwandoxelo27 <138695827+Lwandoxelo27@users.noreply.github.com>
2024-10-17 19:15:37 +0200

    changed the script

--- task_4.sql
+++ task_4.sql
@@ -2,13 +2,21 @@
 
 USE alx_book_store;
 
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, NUMERIC_PRECISION, NUMERIC_SCALE, IS_NULLABLE
+SELECT
+COLUMN_NAME,
+DATA_TYPE,
+CHARACTER_MAXIMUM_LENGTH,
+NUMERIC_PRECISION,
+NUMERIC_SCALE,
+IS_NULLABLE
+FROM
+INFORMATION_SCHEMA.COLUMNS
+WHERE
+TABLE_NAME = 'books';
 
-FROM INFORMATION_SCHEMA.COLUMNS
-
-WHERE TABLE_NAME = 'books';
-
-To run this script and print the full description of the books table from the alx_book_store database, use the following MySQL command:
+To run this script, save it as task_4.sql and execute:
 
 bash
 mysql -u your_username -pyour_password -D alx_book_store < task_4.sql
+
+Replace 'your_username' and 'your_password' with your actual MySQL credentials
