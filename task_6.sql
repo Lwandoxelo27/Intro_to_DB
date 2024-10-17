58f3bad093f2a596c852eae1eb7cf95cf0719df0
Lwandoxelo27 <138695827+Lwandoxelo27@users.noreply.github.com>
2024-10-17 19:28:54 +0200

    inserted script

---
+++ task_6.sql
@@ -0,0 +1,16 @@
+-- task_6.sql
+
+USE alx_book_store;
+
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+
+To run this script, save it as task_6.sql and execute:
+
+bash
+mysql -u your_username -pyour_password -D alx_book_store < task_6.sql
+
+Replace 'your_username' and 'your_password' with your actual MySQL credentials.
