9bbdb3546873347e0e2837916451e6c2f6651aa1
Lwandoxelo27 <138695827+Lwandoxelo27@users.noreply.github.com>
2024-10-17 19:20:58 +0200

    update

--- task_5.sql
+++ task_5.sql
@@ -2,7 +2,7 @@
 
 USE alx_book_store;
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO ustomer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
 
 To run this script, save it as task_5.sql and execute:
